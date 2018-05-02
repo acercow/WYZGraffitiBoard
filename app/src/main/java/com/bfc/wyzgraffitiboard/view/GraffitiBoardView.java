5cfd46e40b007fde8e20fd320196abc88661de85
Acercow <zhaosen123@gmail.com>
2018-05-02 17:22:41 +0800

    相对坐标

--- app/src/main/java/com/bfc/wyzgraffitiboard/view/GraffitiBoardView.java
+++ app/src/main/java/com/bfc/wyzgraffitiboard/view/GraffitiBoardView.java
@@ -1,6 +1,7 @@
 package com.bfc.wyzgraffitiboard.view;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapShader;
@@ -12,6 +13,7 @@ import android.graphics.Path;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -29,6 +31,7 @@ public class GraffitiBoardView extends View {
     private Bitmap mGiftIcon;
     private float mProportion;
     private Matrix mMatrix;
+    private float mCanvasScale;
 
 
     public GraffitiBoardView(Context context) {
@@ -47,15 +50,15 @@ public class GraffitiBoardView extends View {
     }
 
     private void init() {
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setStrokeWidth(10);
-        mPath = new Path();
+//        mPaint = new Paint();
+//        mPaint.setAntiAlias(true);
+//        mPaint.setStrokeWidth(10);
+//        mPath = new Path();
         mGiftIcon = BitmapFactory.decodeResource(getResources(), R.drawable.shield_icon);
-        BitmapShader bitmapShader = new BitmapShader(mGiftIcon, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-//        mPaint.setShader(bitmapShader);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setColor(0xFF992277);
+//        BitmapShader bitmapShader = new BitmapShader(mGiftIcon, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+////        mPaint.setShader(bitmapShader);
+//        mPaint.setStyle(Paint.Style.STROKE);
+//        mPaint.setColor(0xFF992277);
     }
 
     @Override
@@ -65,16 +68,6 @@ public class GraffitiBoardView extends View {
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-        if (mBitmap != null) {
-            if ((mBitmap.getHeight() > heightSize) && (mBitmap.getHeight() > mBitmap.getWidth())) {
-                widthSize = heightSize * mBitmap.getWidth() / mBitmap.getHeight();
-            } else if ((mBitmap.getWidth() > widthSize) && (mBitmap.getWidth() > mBitmap.getHeight())) {
-                heightSize = widthSize * mBitmap.getHeight() / mBitmap.getWidth();
-            } else {
-                heightSize = mBitmap.getHeight();
-                widthSize = mBitmap.getWidth();
-            }
-        }
         Log.d(TAG, "onMeasure: heightSize: " + heightSize + " widthSize: " + widthSize);
         setMeasuredDimension(widthSize, heightSize);
     }
@@ -83,29 +76,29 @@ public class GraffitiBoardView extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         if (mBitmap == null) {
+            mCanvasScale = w / 1080F;
             mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+
+            Resources resources=getResources();
+            DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+            float density = displayMetrics.density;
+            int densityDpi = displayMetrics.densityDpi;
+            System.out.println("----> density=" + density);
+            System.out.println("----> densityDpi=" + densityDpi);
+            Log.i(TAG, "Gift Icon w: " + mGiftIcon.getWidth() + " h: " + mGiftIcon.getHeight());
+
+            Matrix matrix = new Matrix();
+            matrix.postScale(mCanvasScale / density * 3, mCanvasScale / density * 3);
+
+            mGiftIcon = Bitmap.createBitmap(mGiftIcon, 0, 0, mGiftIcon.getWidth(), mGiftIcon.getHeight(), matrix, true);
         }
         mCanvas = new Canvas(mBitmap);
-        mCanvas.drawColor(Color.TRANSPARENT);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        canvas.drawRect(new Rect(0, 0, 400, 400), mPaint);
-//        canvas.drawPath(mPath, mPaint);
-//        canvas.drawColor(Color.BLUE);
-        float proportion = (float) canvas.getHeight() / mBitmap.getHeight();
-        if (proportion < 1) {
-            mProportion = proportion;
-            mMatrix.reset();
-            mMatrix.postScale(proportion, proportion);
-            mMatrix.postTranslate((canvas.getWidth() - mBitmap.getWidth() * proportion) / 2, 0);
-            canvas.drawBitmap(mBitmap, mMatrix, mPaint);
-        } else {
-            mProportion = 0;
-            canvas.drawBitmap(mBitmap, 0, 0, mPaint);
-        }
+        canvas.drawBitmap(mBitmap, 0, 0, null);
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -114,33 +107,15 @@ public class GraffitiBoardView extends View {
         // Checks for the event that occurs
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-//                mPath.moveTo(pointX, pointY);
                 tempX = pointX;
                 tempY = pointY;
                 mCanvas.drawBitmap(mGiftIcon, pointX - mGiftIcon.getWidth() / 2, pointY - mGiftIcon.getHeight() / 2, null);
                 return true;
             case MotionEvent.ACTION_MOVE:
-//                mPath.lineTo(pointX, pointY);
-//                final int historySize = event.getHistorySize();
-//                final int pointerCount = event.getPointerCount();
-//                for (int h = 0; h < historySize; h++) {
-//                        pointX = event.getHistoricalX(h);
-//                        pointY = event.getHistoricalY(h);
-//                        double distance = (int) Math.sqrt(Math.pow(pointX - tempX, 2) + Math.pow(pointY - tempY, 2));
-//                        if (distance > -1) {
-//                            mCanvas.drawBitmap(mGiftIcon, pointX, pointY, null);
-//                            tempX = pointX;
-//                            tempY = pointY;
-//                    }
-//                System.out.printf("At time %d:", event.getEventTime());
-//                for (int p = 0; p < pointerCount; p++) {
-//                    System.out.printf("  pointer %d: (%f,%f)",
-//                            event.getPointerId(p), event.getX(p), event.getY(p));
-//                }
 
                 float distance = (float) Math.sqrt(Math.pow(pointX - tempX, 2) + Math.pow(pointY - tempY, 2));
-                if (distance > 120) {
-                    float ratio = distance / 120F;
+                if (distance > 90F * mCanvasScale) {
+                    float ratio = distance / (90F * mCanvasScale);
                     float gapX = (pointX - tempX) / ratio;
                     float gapY = (pointY - tempY) / ratio;
                     for (int i = 1; i <= ratio; i++) {
@@ -158,24 +133,6 @@ public class GraffitiBoardView extends View {
         // Force a view to draw again
         invalidate();
         return false;
-    }
-
-
-    void printSamples(MotionEvent ev) {
-        final int historySize = ev.getHistorySize();
-        final int pointerCount = ev.getPointerCount();
-        for (int h = 0; h < historySize; h++) {
-            System.out.printf("At time %d:", ev.getHistoricalEventTime(h));
-            for (int p = 0; p < pointerCount; p++) {
-                System.out.printf("  pointer %d: (%f,%f)",
-                        ev.getPointerId(p), ev.getHistoricalX(p, h), ev.getHistoricalY(p, h));
-            }
-        }
-        System.out.printf("At time %d:", ev.getEventTime());
-        for (int p = 0; p < pointerCount; p++) {
-            System.out.printf("  pointer %d: (%f,%f)",
-                    ev.getPointerId(p), ev.getX(p), ev.getY(p));
-        }
     }
 }
 
